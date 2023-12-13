ff04c4b4d63bf7104b8aa3c792f0170c0b1451c8
mjn2max <116730653+mjn2max@users.noreply.github.com>
2023-12-12 22:32:15 -0600

    Add SwiftUI-Extensions library

---
+++ SwiftUI-Extensions/Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftUI-Extensions",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "SwiftUI-Extensions",
+            targets: ["SwiftUI-Extensions"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "SwiftUI-Extensions"),
+        .testTarget(
+            name: "SwiftUI-ExtensionsTests",
+            dependencies: ["SwiftUI-Extensions"]),
+    ]
+)
