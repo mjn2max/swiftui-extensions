56503be2cba38949837a521ce4f02d8a49706ca8
mjn2max <116730653+mjn2max@users.noreply.github.com>
2023-12-14 21:09:29 -0600

    Add examples workspace

---
+++ Examples/iOS-Examples/iOS-Examples/iOS_ExamplesApp.swift
@@ -0,0 +1,21 @@
+//
+// iOS_ExamplesApp.swift
+// iOS-Examples
+//
+// Created by Huy D. on 12/14/23
+// mjn2max.github.io 😜
+// 
+// Copyright © 2023. All rights reserved.
+// ___ORGANIZATIONNAME___
+//
+
+import SwiftUI
+
+@main
+struct iOS_ExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
