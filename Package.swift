abca96f63c93fb1523e83bee292deb4f87fedfb8
Huy D <116730653+mjn2max@users.noreply.github.com>
2024-10-13 22:26:04 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,7 +17,6 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SwiftUI-Extensions",
-            path: "Sources/",
             exclude: [
                 "../Demo/"
             ]
