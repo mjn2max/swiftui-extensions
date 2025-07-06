4287e283ea2e3108e17603f719ca70c396bb70ef
Huy D. <116730653+mjn2max@users.noreply.github.com>
2025-07-06 08:20:44 -0400

    Color+Brightness | Add `func brightnessValue() -> CGFloat`

--- Sources/Colors/Color+Brightness.swift
+++ Sources/Colors/Color+Brightness.swift
@@ -1,5 +1,5 @@
 //
-// File.swift
+// Color+Brightness.swift
 // SwiftUI-Extensions
 //
 // Created by Huy D. on 6/26/25
@@ -9,4 +9,24 @@
 // CodePassion.dev
 //
 
-import Foundation
+import SwiftUI
+
+// MARK: - Color Extensions - Brightness
+
+extension Color {
+    /// Returns the brightness of the color (from 0.0 to 1.0).
+    ///
+    /// # Usage
+    /// ```swift
+    /// let brightness = Color.orange.brightnessValue()
+    /// ```
+    func brightnessValue() -> CGFloat {
+#if os(iOS)
+        var red: CGFloat = 0, green: CGFloat = 0, blue: CGFloat = 0, alpha: CGFloat = 1
+        if UIColor(self).getRed(&red, green: &green, blue: &blue, alpha: &alpha) {
+            return (red * 299 + green * 587 + blue * 114) / 1000
+        }
+#endif
+        return 1.0 // default brightness
+    }
+}
